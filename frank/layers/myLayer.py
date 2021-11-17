ea622358f4968e97a58bbf47e124c67427fedc4a
frank-fn10502 <frank08.chen@gmail.com>
2021-11-17 18:13:36 +0800

    StochasticDropout class move to effNetv2 layer

--- frank/layers/myLayer.py
+++ frank/layers/myLayer.py
@@ -4,28 +4,6 @@ from .other.autoaugment import distort_image_with_randaugment
 
 
 
-# tensorflow.python.framework.ops.EagerTensor' object has no attribute '_keras_history'
-#為了解決以上的錯誤(主要是畫圖時會需要用到該屬性)
-class StochasticDropout(layers.Layer):
-    # from effNetV2 code #survival_prob 預設 0.8 def drop_connect(inputs, is_training = True, survival_prob = 0.8):
-    def call(self, inputs ,training=None):
-        """Drop the entire conv with given survival probability."""
-        # "Deep Networks with Stochastic Depth", https://arxiv.org/pdf/1603.09382.pdf
-        if not training:
-            return inputs
-
-        survival_prob = 0.8
-        # Compute tensor.
-        batch_size = tf.shape(inputs)[0]
-        random_tensor = survival_prob
-        random_tensor += tf.random.uniform([batch_size, 1, 1, 1], dtype=inputs.dtype)
-        binary_tensor = tf.floor(random_tensor)
-        # Unlike conventional way that multiply survival_prob at test time, here we
-        # divide survival_prob at training time, such that no addition compute is
-        # needed at test time.
-        output = inputs / survival_prob * binary_tensor
-        return output
-
 # 使用 google effNetv2 的 code 包裝成 keras.layers
 class DistortImage(layers.Layer):
     '''
@@ -78,7 +56,7 @@ class DistortImage(layers.Layer):
         else:
             return inputs
 
-class Inception(layers.Layer):
+class InceptionBlock(layers.Layer):
     def __init__(self, *depth):
         super().__init__()
         self._build(depth)
